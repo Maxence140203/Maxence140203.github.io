23f964bdec1e16a23eca1ab9332110bae324af40
Maxence <maxence.poizat@etu.uca.fr>
2023-03-23 20:58:13 +0100

    SAE_IMPLE

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 $(document).ready(function() {
-    var slideCount = $('.slide').length;
-    var currentIndex = 0;
-  
+  var slideCount = $('.slide').length;
+  var currentIndex = 0;
+
+  $('.slide').hide();
+  $('.slide:first').show();
+
+  $('.prev').click(function() {
+    currentIndex -= 1;
+    if (currentIndex < 0) {
+      currentIndex = slideCount - 1;
+    }
     $('.slide').hide();
-    $('.slide:first').show();
-  
-    $('.prev').click(function() {
-      currentIndex -= 1;
-      if (currentIndex < 0) {
-        currentIndex = slideCount - 1;
-      }
-      $('.slide').hide();
-      $('.slide:eq(' + currentIndex + ')').show();
-    });
-  
-    $('.next').click(function() {
-      currentIndex += 1;
-      if (currentIndex > slideCount - 1) {
-        currentIndex = 0;
-      }
-      $('.slide').hide();
-      $('.slide:eq(' + currentIndex + ')').show();
-    });
+    $('.slide:eq(' + currentIndex + ')').show();
   });
+
+  $('.next').click(function() {
+    currentIndex += 1;
+    if (currentIndex > slideCount - 1) {
+      currentIndex = 0;
+    }
+    $('.slide').hide();
+    $('.slide:eq(' + currentIndex + ')').show();
+  });
+});
+
+$(document).ready(function() {
+  var slideCount = $('.slide2').length;
+  var currentIndex = 0;
+
+  $('.slide2').hide();
+  $('.slide2:first').show();
+
+  $('.prev2').click(function() {
+    currentIndex -= 1;
+    if (currentIndex < 0) {
+      currentIndex = slideCount - 1;
+    }
+    $('.slide2').hide();
+    $('.slide2:eq(' + currentIndex + ')').show();
+  });
+
+  $('.next2').click(function() {
+    currentIndex += 1;
+    if (currentIndex > slideCount - 1) {
+      currentIndex = 0;
+    }
+    $('.slide2').hide();
+    $('.slide2:eq(' + currentIndex + ')').show();
+  });
+});
