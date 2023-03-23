f1c01159e5f8dde41199362e1f8d24ac5d748fe9
Maxence <maxence.poizat@etu.uca.fr>
2023-03-24 00:17:52 +0100

    Changement CV

--- Portfolio/result.js
+++ Portfolio/result.js
@@ -15,8 +15,8 @@ const getCv = () =>{
   terminal.insertAdjacentHTML("beforeend", 
         `<p class="result cvresult">${data[language].cv}</p>`);
   const a = document.createElement("a");
-  a.href = "ressources/CV_Poizat_Maxence.pdf";
-  a.setAttribute("download", "CV_Poizat_Maxence.pdf");
+  a.href = "ressources/CV_Maxence_Poizat.pdf";
+  a.setAttribute("download", "CV_Maxence_Poizat.pdf");
   a.setAttribute("target", "blank");
   a.click();
 }
